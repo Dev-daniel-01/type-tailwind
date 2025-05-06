2c2c522986e0c130677eb7a5b5cf19bfe8d28c47
Daniel Ribeiro da Costa <ribeirodacostadaniel57@gmail.com>
2025-05-06 00:01:44 -0300

    Add files via upload

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -11,13 +11,12 @@ interface MenuProps {
 export const Menu = ({op1, op2, op3, op4} : MenuProps) => {
   return(
     <header >
-      <nav className="flex p-[0.5rem] gap-[1rem]">
-        <Link className="font-black " href={"/"}>{op1}</Link>
-        <Link className="font-black" href={"/hookPage"}>{op2}</Link>
-        <Link className="font-black" href={"/axiosPage"}>{op3}</Link>
-        <Link className="font-black" href={"/serverSide"}>{op4}</Link>
+      <nav className="fixed flex justify-center p-[0.5rem] gap-[1rem] z-999 w-[100vw] bg-[white]">
+        <Link className="font-black opacity-[0.6] hover:opacity-[1.0]" href={"/"}>{op1}</Link>
+        <Link className="font-black opacity-[0.6] hover:opacity-[1.0]" href={"/hookPage"}>{op2}</Link>
+        <Link className="font-black opacity-[0.6] hover:opacity-[1.0]" href={"/axiosPage"}>{op3}</Link>
+        <Link className="font-black opacity-[0.6] hover:opacity-[1.0]" href={"/serverSide"}>{op4}</Link>
       </nav>
     </header>
-
   )
 }
